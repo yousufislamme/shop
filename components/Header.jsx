e4b91c6041f3c605e1bd84fe6fa6b36aeddd5a63
yousufislamme <online.okk@gmail.com>
2024-02-12 09:45:29 +0600

    Header cart icon add and some change

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,24 +1,46 @@
+"use client";
+import { Context } from "@/context";
 import Link from "next/link";
-
+import { useContext } from "react";
+import { IoMdCart } from "react-icons/io";
 const Header = () => {
-   return (
-      <header>
-         <div className="items-center justify-between px-10 py-3 bg-slate-700 flex text-white">
-            <div>
-               <Link href="/">
-                  <h1>Logo</h1>
-               </Link>
-            </div>
-            <div>
-               <ul className="flex gap-5">
-                  <li><Link href='/products'>Products</Link></li>
-                  <li><Link href='/about'>About</Link></li>
-                  <li><Link href='/cart'>Cart</Link></li>
-               </ul>
-            </div>
-         </div>
-      </header>
-   );
+  const { cartItems } = useContext(Context);
+  const totalProductLength = cartItems.length;
+  return (
+    <header>
+      <div className="flex items-center justify-between bg-slate-700 px-10 py-3 text-white">
+        <div>
+          <Link href="/">
+            <h1>Logo</h1>
+          </Link>
+        </div>
+        <div>
+          <ul className="flex gap-5">
+            <li>
+              <Link href="/products">Products</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link
+                className="flex items-center justify-center gap-1"
+                href="/cart"
+              >
+                <span>
+                  <IoMdCart className="text-xl" />
+                </span>
+                <span className="flex h-5 w-5 items-center justify-center rounded-full bg-red-500 text-sm text-white">
+                  {totalProductLength}
+                </span>{" "}
+                Cart
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
